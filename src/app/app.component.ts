439c09b9e5e73b58be515fb2994a48aee30b179c
ISAYA YOKOYAMA <isaya4580@icloud.com>
2017-12-25 23:27:48 +0900

    layouts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  
+
 }
