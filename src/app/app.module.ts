10f1d877beb7313657cfa23e55c1ebdefd0f40af
ISAYA YOKOYAMA <isaya4580@icloud.com>
2017-12-28 00:27:15 +0900

    tabletest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { AngularSplitModule } from 'angular-split';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppMainComponent } from './app-main/app-main.component';
+import { TableTestComponent } from './table-test/table-test.component';
+
+import { AngularSplitModule } from 'angular-split';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppMainComponent
+    AppMainComponent,
+    TableTestComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AngularSplitModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
