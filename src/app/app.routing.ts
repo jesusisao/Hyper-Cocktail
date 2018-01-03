338cc87c045e9ccfce0d3edde7392814def6ad91
yokoyanh <kachintokita@yahoo.co.jp>
2018-01-03 10:35:45 +0900

    fix

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,8 +5,14 @@ import { ErrorComponent } from './error/error.component';
 import { SubmenuTableTestComponent } from './submenu-table-test/submenu-table-test.component';
 import { SubmenuFileGeneratorComponent } from './submenu-file-generator/submenu-file-generator.component';
 
+import { MatTableTestComponent } from './mat-table-test/mat-table-test.component';
+
+//メインメニューアイコンを押したときにサブメニューを書き換える
 const myRoutes = [
+    { path: '', component: SubmenuFileGeneratorComponent },
     { path: 'table-test', component: SubmenuTableTestComponent },
+    { path: 'table-test/mat-table-test', component: MatTableTestComponent, outlet: 'content-main' },
+    { path: 'table-test/**', component: ErrorComponent, outlet: 'content-main' },
     { path: 'file-generator', component: SubmenuFileGeneratorComponent },
     { path: '**', component: ErrorComponent },
 ]
--- src/app/submenu-table-test/submenu-table-test.component.ts
+++ src/app/submenu-table-test/submenu-table-test.component.ts
@@ -1,32 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-submenu-table-test',
   templateUrl: './submenu-table-test.component.html',
   styleUrls: ['./submenu-table-test.component.scss']
 })
-export class SubmenuTableTestComponent implements OnInit {
+export class SubmenuTableTestComponent {
 
   submenues: Submenu[] = SUBMENUES;
-  
-  constructor() { }
 
-  ngOnInit() {
+  //親コンポーネントのrouter-outletで遷移させるために
+  //パスを送り込む
+  //ダメ。サービスに切り替える
+  @Output() clickSubmenu = new EventEmitter<String>();
+
+  onclick(routeOfSubmenu: string) {
+    this.clickSubmenu.emit(routeOfSubmenu);
   }
 }
 
 export interface Submenu {
-  id:          number;
-  name:        string;
+  id: number;
+  name: string;
+  route: string;
   description: string;
-  sortNum:     number;
+  sortNum: number;
 }
 
 export const SUBMENUES: Submenu[] = [
-  { id: 100, name: 'Mat Table Test', description: 'テスト説明文', sortNum: 10},
-  { id: 101, name: 'test1', description: 'テスト説明文', sortNum: 11},
-  { id: 102, name: 'test2', description: 'テスト説明文', sortNum: 12},
-  { id: 103, name: 'test3', description: 'テスト説明文', sortNum: 13},
-  { id: 104, name: 'test4', description: 'テスト説明文', sortNum: 14}
+  { id: 100, name: 'Mat Table Test', route: '/table-test/mat-table-test', description: 'テスト説明文', sortNum: 10 },
+  { id: 101, name: 'test1', route: 'table-test/other-table-test', description: 'テスト説明文', sortNum: 11 },
+  { id: 102, name: 'test2', route: 'table-test/other-table-test', description: 'テスト説明文', sortNum: 12 },
+  { id: 103, name: 'test3', route: 'table-test/other-table-test', description: 'テスト説明文', sortNum: 13 },
+  { id: 104, name: 'test4', route: 'table-test/other-table-test', description: 'テスト説明文', sortNum: 14 }
 ]
 
