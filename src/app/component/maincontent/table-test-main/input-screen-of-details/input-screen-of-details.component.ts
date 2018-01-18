65d22fdffd989da7cfcc9e080d82bf695112eb1a
ISAYA YOKOYAMA <kachintokita@yahoo.co.jp>
2018-01-19 00:16:40 +0900

    details fix

--- src/app/component/maincontent/table-test-main/input-screen-of-details/input-screen-of-details.component.ts
+++ src/app/component/maincontent/table-test-main/input-screen-of-details/input-screen-of-details.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputScreenOfDetailsComponent implements OnInit {
 
+  rows: number[] = [0, 0, 0, 0, 0];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // 最後に選択した行番号rowindexを持っておいて、そいつを基準に挿入したり削除したりすればいい
+
+  addButtonClicked() {
+    this.rows.push(0);
+  }
+
+  removeButtonClicked() {
+    this.rows.pop();
   }
 
 }
