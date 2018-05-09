16f476c473601b234263e40ec13d3e2e0fa43de4
ISAYA YOKOYAMA <kachintokita@yahoo.co.jp>
2018-05-09 23:51:31 +0900

    mysql config

--- server/index.js
+++ server/index.js
@@ -3,43 +3,11 @@ const compression = require('compression');
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
-const mysql = require("mysql");
-const md5 = require('MD5');
+const bodyParser = require('body-parser')
 
-function REST() {
-    const self = this;
-    self.connectMysql();
-};
-
-REST.prototype.connectMysql = function () {
-    const self = this;
-    const pool = mysql.createPool({
-        connectionLimit: 100,
-        host: 'localhost',
-        user: 'db user name',
-        password: 'db password',
-        database: 'Test',
-        debug: false
-    });
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            self.stop(err);
-        } else {
-            self.configureExpress(connection);
-        }
-    });
-}
-
-REST.prototype.configureExpress = function (connection) {
-    const self = this;
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-    const router = express.Router();
-    app.use('/api', router);
-    const rest_router = new rest(router, connection, md5);
-    self.startServer();
-}
+// postの前準備。expressはそのままではリクエストのbodyをパースできないため。
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // 静的リソースの圧縮をgzipで行う。圧縮レベルはzlibのデフォルトの6。
 app.use(compression());
@@ -50,10 +18,11 @@ app.use(express.static(__dirname + '/../dist'));
 
 // Start the app by listening on the default
 // Heroku port
+const port = process.env.PORT || 8080;
 app.listen(port);
 
 // API実装テスト
-app.get('/api/:id', function (req, res) {
+app.get('/api/user/:id', function (req, res) {
     let user = {
         id: req.params.id,
         name: "tanaka",
---
+++ server/mysql-connect.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+// JAWSDB_URLはJAWSDBを入れた時にHeroku側でデフォルトで作られるNodeの環境変数。
+// そいつの中に接続情報が入ってる。
+const pool = mysql.createPool(process.env.JAWSDB_URL + '?connectionLimit=100' + '?debug=false');
+
+pool.getConnection(function (err, connection) {
+    if (err) {
+        console.log("ISSUE WITH MYSQL n" + err);
+    } else {
+        console.log("CONNECTED TO MYSQL.");
+    }
+});
