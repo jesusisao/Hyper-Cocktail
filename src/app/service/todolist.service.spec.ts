1950565f2183a5df29cad20daceca309f965e193
yokoyanh <kachintokita@yahoo.co.jp>
2018-06-15 09:12:25 +0900

    test fixed

--- src/app/service/todolist.service.spec.ts
+++ src/app/service/todolist.service.spec.ts
@@ -1,15 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { HttpModule } from '@angular/http';
 import { TodolistService } from './todolist.service';
 
-// describe('TodolistService', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [TodolistService]
-//     });
-//   });
+describe('TodolistService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        providers: [TodolistService],
+        imports: [HttpModule],
+    });
+  });
 
-//   it('should be created', inject([TodolistService], (service: TodolistService) => {
-//     expect(service).toBeTruthy();
-//   }));
-// });
+  it('should be created', inject([TodolistService], (service: TodolistService) => {
+    expect(service).toBeTruthy();
+  }));
+});
--- src/app/service/zipcode.service.spec.ts
+++ src/app/service/zipcode.service.spec.ts
@@ -1,9 +1,19 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientJsonpModule, HttpBackend, JsonpClientBackend } from '@angular/common/http';
+// import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ZipcodeService } from './zipcode.service';
+import { JsonpModule } from '@angular/http';
+
 describe('ZipcodeService', () => {
+
   beforeEach(() => {
-    // 初期化処理
+    TestBed.configureTestingModule({
+      providers: [ZipcodeService, { provide: JsonpClientBackend, useExisting: HttpBackend }],
+      imports: [JsonpModule], // 直で読み込むのが良いことなのか分からない
+    });
   });
 
-  it('テストその１', () => {
-    expect(1 + 1).toEqual(2);
-  });
+  it('should be created', inject([ZipcodeService], (service: ZipcodeService) => {
+    expect(service).toBeTruthy();
+  }));
 });
